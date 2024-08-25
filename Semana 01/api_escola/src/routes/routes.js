ff6a6e8295dae5e28f7a3ef219cc1b78269f716f
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2024-08-25 16:06:20 -0300

    Add auth.routes e atualizado o routes.js

--- Semana 01/api_escola/src/routes/routes.js
+++ Semana 01/api_escola/src/routes/routes.js
@@ -3,12 +3,17 @@ const alunoRoutes = require("./alunos.route");
 const cursoRoutes = require("./cursos.route");
 const loginRoutes = require("./login.route");
 const matriculaRoutes = require("./matricula.route");
+const rbacRoutes = require("./rbac.routes");
+const authRoutes = require('./auth.routes');
+const userRoutes = require('./usuarios.routes');
 
 const routes = Router()
 
 routes.use('/matriculas', matriculaRoutes)
 routes.use('/alunos', alunoRoutes)
 routes.use('/cursos', cursoRoutes)
-routes.use('/login', loginRoutes)
+// routes.use('/login', loginRoutes)
+routes.use('/login', authRoutes)
+routes.use('/rbac', rbacRoutes)
 
 module.exports = routes
