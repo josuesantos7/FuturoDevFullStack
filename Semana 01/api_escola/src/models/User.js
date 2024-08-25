976fc61c471cfba30498f6b55990d89ede387cb5
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2024-08-25 00:47:14 -0300

    Add models Role, Permission, UserRole, PermissionRole e a migration create-rbac.js

--- Semana 01/api_escola/src/models/User.js
+++ Semana 01/api_escola/src/models/User.js
@@ -4,6 +4,11 @@ const { connection }  = require("../database/connection")
 const { Datatypes } = require ("sequelize")
 
 const User = connection.define("user", {
+    id: {
+        type: Datatypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
     name: {
         type: Datatypes.STRING
     },
